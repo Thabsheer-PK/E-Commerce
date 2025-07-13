f4e3ecfd62e12c46ecba35d47b49d1d1e67929e1
Thabsheer <thaburazz@gmail.com>
2025-07-13 12:41:53 +0530

    create admin view product in table format

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
 var exphbs = require('express-handlebars')
 
 var app = express();
@@ -17,7 +17,12 @@ app.engine('hbs',exphbs.engine({
   extname: 'hbs',
   defaultLayout: 'layout',
   layoutsDir: __dirname+'/views/layout',
-  partialsDir: __dirname+'/views/partials'
+  partialsDir: __dirname+'/views/partials',
+  helpers: {
+    inc: function(value){
+      return parseInt(value) + 1;
+    }
+  }
 }))
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,8 +30,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
