a3fbe3c0e8b78f2ce0d20d3c34bafc5c74316641
Thabsheer <thaburazz@gmail.com>
2025-07-15 16:07:11 +0530

    delete button implemented

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,21 +1,30 @@
 const { getDB } = require('../config/connect')
 const collection = require('../config/collections')
+const { ObjectId } = require('mongodb')
 
 module.exports = {
   addProduct: async (product) => {
     await getDB().collection(collection.PRODUCT_COLLECTION).insertOne(product);
     return product._id.toString();//this return image id
   },
-  getAllProducts:()=>{ // can deal with asyn or callback or promise ,now we with deal promise
-    return new Promise (async (resolve,reject)=>{
+  getAllProducts: () => { // can deal with asyn or callback or promise ,now we with deal promise
+    return new Promise(async (resolve, reject) => {
       let products = await getDB().collection(collection.PRODUCT_COLLECTION).find().toArray();
       //prodcts _id in object mode, we want convert into string
-      products = products.map((product)=>{
-        return{
-          ...product,_id: product._id.toString()
+      products = products.map((product) => {
+        return {
+          ...product, _id: product._id.toString()
         }
       })
       resolve(products)
+    })
+  },
+  deleteProduct: (productID) => {
+    return new Promise((resolve, reject) => {
+      getDB().collection(collection.PRODUCT_COLLECTION).deleteOne({ _id: new ObjectId(productID) }).then((response) => {
+        resolve(response);
+      })
+
     })
   }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -5,10 +5,9 @@ const productHelpers = require('../helpers/product-helpers');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  productHelpers.getAllProducts().then((products)=>{
+  productHelpers.getAllProducts().then((products) => {
     res.render('admin/view-products', { admin: true, products })
   })
-  
 });
 
 router.get('/add-product', (req, res) => {
@@ -16,13 +15,21 @@ router.get('/add-product', (req, res) => {
 })
 
 router.post('/add-product', async (req, res) => {
- 
   const id = await productHelpers.addProduct(req.body) //id have image id
-  
   let image = req.files.Image;
   await image.mv('./public/product-images/' + id + '.jpg')//imag save this path
   res.render('admin/add-product-form', { admin: true });
-
 })
+
+router.get('/deleteProduct/:id',(req,res,next)=>{
+  let productID = req.params.id;
+  productHelpers.deleteProduct(productID).then((response)=>{
+    console.log(response);
+    res.redirect('/admin/')
+  })
+  
+  
+})
+
 
 module.exports = router;
